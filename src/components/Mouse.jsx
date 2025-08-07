c846b372b36878db6b9faad591ea87aa64519335
himusinggit <hbilluscoding@gmail.com>
2025-08-07 12:17:42 +0530

    custom cursor

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -4,18 +4,51 @@ import { useGSAP } from '@gsap/react';
 
 function Mouse() {
     const mouse = useRef(null);
+    const trail = useRef(null);
+    const wrap = useRef(null);
     useEffect(() => {
-            window.addEventListener('mousemove', e => {
-        gsap.to(mouse.current, {duration: 3,
-            ease: "elastic.out(1,0.3)",
-            x: e.clientX+20,
-            y: e.clientY+20
-        });
+  if (wrap.current) {
+    wrap.current.querySelectorAll('.trailets').forEach(el => el.remove());
+  }
+              for (let i = 0; i < 15; i++) {
+        const size = 19 - i; // from 19px down to 5px
+        const div = document.createElement('div');
+        div.style.width = size + 'px';
+        div.style.height = size + 'px';
+        div.className = 'trailets';
+        div.style.backgroundColor = 'yellow';
+        div.style.borderRadius = '50%';
+        div.style.display = 'inline-block';
+        div.style.margin = '4px';
+        div.style.transform='translate(-50%, -50%)';
+        wrap.current.appendChild(div);
+      }
+    const handleMouseMove = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    mouse.current.style.transform = `translate(${x}px, ${y}px)`;
+    trail.current.style.transform = `translate(${x + 32}px, ${y + 32}px)`;
+
+    gsap.to('.trailets', {
+      duration: 0.1,
+      x:x+38,
+      y:y+18,
+      stagger: 0.006,
+      ease: 'power2.out',
+    });
+  };
+window.addEventListener('mousemove', handleMouseMove);
+    return (() => {
+        window.removeEventListener('mousemove', handleMouseMove);
     })
+    
     },[])
 
-    return (
+    return (<div ref={wrap}>
         <div ref = {mouse} className='mouseFollower'></div>
+        <div ref={trail} className="trail"></div>
+        </div>
     )
 }
 
